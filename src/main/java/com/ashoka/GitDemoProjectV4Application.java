08054b16bde708facbb7bde8279857893653b92a
ashoka <ashoka.devara89@gmail.com>
2020-04-02 09:05:50 +0530

    First Commit

---
+++ src/main/java/com/ashoka/GitDemoProjectV4Application.java
@@ -0,0 +1,13 @@
+package com.ashoka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProjectV4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoProjectV4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashoka/GitDemoProjectV4/GitDemoProjectV4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashoka.GitDemoProjectV4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoProjectV4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
